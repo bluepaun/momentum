65d16472bb721f271baa3340a9e8bdcfc3d91987
bluepaun <bluepaun@gmail.com>
2022-10-20 23:08:19 +0800

    Update 2022-10-20T15:08:04.760Z

--- js/app.js
+++ js/app.js
@@ -2519,7 +2519,7 @@ function updateWeather(city, text, icon, temp) {
   const iconImg = document.createElement("img");
 
   if (icon !== undefined) {
-    iconImg.src = `http://openweathermap.org/img/wn/${icon}@4x.png`;
+    iconImg.src = `https://openweathermap.org/img/wn/${icon}@4x.png`;
   }
 
   const describtion = document.createElement("span");
